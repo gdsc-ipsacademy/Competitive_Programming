c2060b45a03f8d1b25278195a0639802a44d538f
haki-user <adityapratapsingh33@outlook.com>
2023-10-03 22:44:45 +0530

    modified:   binary search.cpp

--- algorithms/binary-search/binary search.cpp
+++ algorithms/binary-search/binary search.cpp
@@ -1,23 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-template<typename T> 
+template<typename T>
 int binary_search(vector<T> &v, T x) {
-    int l = 0, r = v.size() - 1;
-    while (l <= r) {
-        int mid = l + (r-l >> 1);
-        if (v[mid] == x) return mid;
-        else if (v[mid] < x) l = mid + 1;
-        else r = mid - 1;
+    int left = 0;
+    int right = v.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (v[mid] == x) {
+            return mid; // Target found
+        } else if (v[mid] < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-    return -1;
+
+    return -1; // Target not found
 }
 
 int main() {
     vector<int> v = {1, 2, 3, 4, 5};
-    int x = 3;
-    cin>>x;
+    int x = 3; 
+
     int index_x = binary_search(v, x);
-    cout << index_x << endl;
+
+    if (index_x != -1) {
+        cout << "Target " << x << " found at index " << index_x << endl;
+    } else {
+        cout << "Target " << x << " not found in the array." << endl;
+    }
+
     return 0;
 }
