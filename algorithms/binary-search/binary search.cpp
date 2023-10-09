01f612d97f6c43b4f3eda732e0a28f74b409dbd2
haki-user <adityapratapsingh33@outlook.com>
2023-10-09 20:44:45 +0530

    workflow test

--- algorithms/binary-search/binary search.cpp
+++ algorithms/binary-search/binary search.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T>
-int binary_search(vector<T> &v, T x) {
+template <typename T>
+int binary_search(vector<T> &v, T x){
     int left = 0;
     int right = v.size() - 1;
 
@@ -22,7 +22,7 @@ int binary_search(vector<T> &v, T x) {
 
 int main() {
     vector<int> v = {1, 2, 3, 4, 5};
-    int x = 3; 
+    int x = 3;
 
     int index_x = binary_search(v, x);
 
