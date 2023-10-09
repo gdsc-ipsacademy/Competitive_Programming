c0706d9641a7d86ec033ce5a1573377a2d3b5014
Thejas0604 <meddepolamact.21@uom.lk>
2023-10-09 15:07:46 +0530

    added fibonacci in cpp snippet(code template for fibonacci in cpp).

---
+++ cp templates/cpp/fibonacci-snippet.cpp
@@ -0,0 +1,12 @@
+// recursive fibonacci function(n>=0)
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
