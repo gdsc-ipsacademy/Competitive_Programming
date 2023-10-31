1fec0974d773213d765d499d1e0abc2ceda050a8
Abhishek <yadavabhishek9503@gmail.com>
2023-10-31 13:01:50 +0530

    Updated

--- data structures/Map and Tries/Lecture2.cpp
+++ data structures/Map and Tries/Lecture2.cpp
@@ -1,3 +1,109 @@
+// #include <iostream>
+// using namespace std;
+
+// class TrieNode
+// {
+// public:
+//     char data;
+//     TrieNode *children[25];
+
+//     bool isTerminal;
+
+//     TrieNode(char d)
+//     {
+//         this->data = d;
+//         for (int i = 0; i < 26; i++)
+//         {
+//             children[i] = NULL;
+//         }
+//         this->isTerminal = false;
+//     }
+// };
+
+// void insertWord(TrieNode *root, string word)
+// {
+
+//     // cout << "Inserting " << word << endl;
+//     // base case
+//     if (word.length() == 0)
+//     {
+//         root->isTerminal = true;
+//         return;
+//     }
+//     char ch = word[0];
+//     int index = ch - 'a';
+//     TrieNode *child;
+
+//     // present
+//     if (root->children[index] != NULL)
+//     {
+//         child = root->children[index];
+//     }
+//     else
+//     {
+//         // not present
+//         child = new TrieNode(ch);
+//         root->children[index] = child;
+//     }
+//     // recursion sabhal lega
+
+//     insertWord(child, word.substr(1));
+// }
+
+// bool searchWord(TrieNode *root, string word)
+// {
+//     // base case
+//     if (word.length() == 0)
+//     {
+//         return root->isTerminal;
+//     }
+
+//     char ch = word[0];
+//     int index = ch - 'a';
+//     TrieNode *child;
+
+//     // present
+//     if (root->children[index] != NULL)
+//     {
+//         child = root->children[index];
+//     }
+//     else
+//     {
+//         return false;
+//     }
+
+//     // rec call
+
+//     searchWord(child, word.substr(1));
+// }
+
+// int main()
+// {
+
+//     TrieNode *root = new TrieNode('-');
+
+//     // root-.inserword("coding");
+//     insertWord(root, "coding");
+//     insertWord(root, "code");
+//     insertWord(root, "coder");
+//     insertWord(root, "codehelp");
+//     insertWord(root, "baba");
+//     insertWord(root, "babu");
+
+//     if (searchWord(root, "babu"))
+//     {
+//         cout << "Present " << endl;
+//     }
+
+//     else
+//     {
+//         cout << "Not present" << endl;
+//     }
+
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
@@ -5,7 +111,7 @@ class TrieNode
 {
 public:
     char data;
-    TrieNode *children[25];
+    TrieNode *children[26]; // Changed 25 to 26 to account for all English lowercase letters
 
     bool isTerminal;
 
@@ -22,67 +128,58 @@ public:
 
 void insertWord(TrieNode *root, string word)
 {
-
-    // cout << "Inserting " << word << endl;
-    // base case
+    // Base case
     if (word.length() == 0)
     {
         root->isTerminal = true;
         return;
     }
+    
     char ch = word[0];
     int index = ch - 'a';
     TrieNode *child;
-
-    // present
+    
     if (root->children[index] != NULL)
     {
         child = root->children[index];
     }
     else
     {
-        // not present
         child = new TrieNode(ch);
         root->children[index] = child;
     }
-    // recursion sabhal lega
-
+    
     insertWord(child, word.substr(1));
 }
 
 bool searchWord(TrieNode *root, string word)
 {
-    // base case
+    // Base case
     if (word.length() == 0)
     {
         return root->isTerminal;
     }
-
+    
     char ch = word[0];
     int index = ch - 'a';
     TrieNode *child;
-
-    // present
+    
     if (root->children[index] != NULL)
     {
         child = root->children[index];
+        // You need to return the result of the recursive call.
+        return searchWord(child, word.substr(1));
     }
     else
     {
         return false;
     }
-
-    // rec call
-
-    searchWord(child, word.substr(1));
 }
 
 int main()
 {
-
     TrieNode *root = new TrieNode('-');
 
-    // root-.inserword("coding");
     insertWord(root, "coding");
     insertWord(root, "code");
     insertWord(root, "coder");
@@ -92,9 +189,8 @@ int main()
 
     if (searchWord(root, "babu"))
     {
-        cout << "Present " << endl;
+        cout << "Present" << endl;
     }
-
     else
     {
         cout << "Not present" << endl;
